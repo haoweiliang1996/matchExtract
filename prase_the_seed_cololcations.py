585b927fa0275829d7a0255d98d47edbe7be9488
Scottliang <liang199611@outlook.com>
2016-11-19 17:12:41 +0800

    简单处理了种子库

--- prase_the_seed_cololcations.py
+++ prase_the_seed_cololcations.py
@@ -1,6 +1,6 @@
 import re
 
-with open('collocation.in','r') as f:
+with open('原料/collocation.in','r') as f:
     f_lines=list(map(lambda x:x.strip('\n'),f.readlines()))
 
 #去掉注释
@@ -9,6 +9,10 @@ f_lines=list(filter(lambda x:re.search('//',x)==None and len(x)>0,f_lines))
 f_lines = list(map(lambda x: re.sub('!|词类', '', x), f_lines))
 
 f_lines = list(map(lambda x: re.sub('>|<', ' ', x), f_lines))
+
+for i in range(len(f_lines)):
+    f_lines[i]=f_lines[i][1:]
+f_lines=list(map(lambda x:re.sub('\* ','*',x),f_lines))
 with open('0.in','w') as f:
     for line in f_lines:
         f.write(line+'\n')
